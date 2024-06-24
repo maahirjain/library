beef4ebb9f715ad9af82ff31341de985878fa326
Maahir Jain <maahirdjain@gmail.com>
2024-06-25 02:06:33 +0530

    Create Book constructor in JS

---
+++ script.js
@@ -0,0 +1,11 @@
+const myLibrary = [];
+
+function Book(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+}
+
+function addBookToLibrary(book) { myLibrary.push(book); }
+function removeBookFromLibrary(index) { myLibrary.splice(index, 1); }
